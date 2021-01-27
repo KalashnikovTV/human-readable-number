0d7b70f2144cd68dc84653bf9d432bbc68c7ba3b
KalashnikovTV <max.malaschkov@mail.ru>
2021-01-27 11:05:28 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (number === 0) {
+        return 'zero';
+    }
+
+    const hundr = Math.floor(number / 100);
+    let tens = Math.floor((number - hundr * 100) / 10);
+    let ones = number - hundr * 100 - tens * 10;
+
+    if (tens === 1) {
+        tens = 0;
+        ones = number - hundr * 100;
+    }
+
+    const onesArr = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tensArr = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+
+    const hundrArr = hundr ? onesArr[hundr] + ' hundred ' : '';
+
+    return (hundrArr + tensArr[tens] + onesArr[ones]).trim();
+};
